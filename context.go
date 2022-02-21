fe78ea678944245cbd9cc53fa06abb3c48ec35a2
dingx.li <dingx.li@sunyard.com>
2022-02-21 09:12:18 +0800

    remove log

--- context.go
+++ context.go
@@ -8,7 +8,6 @@ import (
 	"image/jpeg"
 	"image/png"
 	"io"
-	"log"
 	"math"
 	"strings"
 
@@ -786,7 +785,6 @@ func (dc *Context) DrawStringWrapped(s string, x, y, ax, ay, width, lineSpacing 
 		x += width
 	}
 	ay = 1
-	log.Println(dc.fontHeight * lineSpacing * float64(len(lines)))
 	for _, line := range lines {
 		dc.DrawStringAnchored(line, x, y, ax, ay)
 		y += dc.fontHeight * lineSpacing
